45dd3bfb8f1af6347516992e434b36a56e6a3af6
Malte Schwarzkopf <malte@cs.brown.edu>
2024-04-11 19:26:14 -0400

    Update section 7 code

--- sec7/pipes.c
+++ sec7/pipes.c
@@ -6,50 +6,53 @@
 #include <time.h>
 #include <unistd.h>
 
-#define NUM_CHILDREN 5
-
 int main() {
   // Create the pipes
   int pipe_fds[2];
   pipe(pipe_fds);
 
-  // Spawn child processes which run their subroutines
-  for (int i = 0; i < NUM_CHILDREN; i++) {
-    pid_t pid = fork();
+  // Spawn child process
+  pid_t pid = fork();
 
-    // Child code
-    if (pid == 0) {
-      // Close the read end of pipe (children won't read)
-      close(pipe_fds[0]);
+  // Child code
+  if (pid == 0) {
+    // Close the read end of pipe (children won't read)
+    close(pipe_fds[0]);
 
-      // Write the child PID
-      pid_t child_pid = getpid();
-      write(pipe_fds[1], &child_pid, sizeof(pid_t));
+    int grandchild_pids[5];
 
-      // This child is done, close the file descriptor
-      close(pipe_fds[1]);
-      return 0;
+    for (int i = 0; i < 5; i++) {
+      grandchild_pids[i] = fork();
+      if (grandchild_pids[i] == 0) {
+        exit(0);
+      }
     }
 
-    // No parent code in the for loop
+    // Write the child PIDs
+    for (int i = 0; i < 5; i++) {
+      int gc_pid = grandchild_pids[i];
+      write(pipe_fds[1], &gc_pid, sizeof(pid_t));
+    }
+
+    // This child is done, close the file descriptor and exit
+    close(pipe_fds[1]);
+    return 0;
   }
 
   // Parent code
   // Close write end of pipe
   close(pipe_fds[1]);
 
-  // Keep on reading data until all child processes have closed write ends
+  // Keep on reading data until child process has closed write ends
   // If the pipe is empty and not closed, call will block
   pid_t curr_pid;
   while (read(pipe_fds[0], &curr_pid, sizeof(curr_pid))) {
     printf("Received pid %d.\n", curr_pid);
   }
 
-  // Wait for all child processes to terminate
-  for (int i = 0; i < NUM_CHILDREN; i++) {
-    int status;
-    wait(&status);
-  }
+  // Wait for child process to terminate
+  int status;
+  wait(&status);
 
   // Close remaining file descriptors; pipe memory is on stack, so it is
   // automatically cleaned up
